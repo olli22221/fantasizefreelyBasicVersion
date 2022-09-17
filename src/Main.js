a62b946606d530a4a8893a0c7c7ea31dd0007b38
Oliver Pohl <olli22221@web.de>
2022-09-17 18:14:18 +0200

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,145 @@
+import React from "react";
+import './css/Main.css';
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { jwtToken as jwtTokenAtom, originalityScore as originalityScoreAtom,
+flexabilityScore as flexabilityScoreAtom, fluencyScore as fluencyScoreAtom, submissions as submissionsAtom} from "./redux/store";
+import  { useState,useCallback,useEffect } from 'react';
+import Typewriter from 'typewriter-effect';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useAlert } from 'react-alert'
+
+
+const Button = styled.button`
+    background-color: blue;
+    font-size: 30px;
+    padding: 100px 60px;
+    color: white;
+    background-color:rgb(190, 165, 132);
+`
+
+
+function Main() {
+    const [jwtToken, setjwtToken] = useRecoilState(jwtTokenAtom);
+
+    const [originalityScore, setOriginalityScore] = useRecoilState(originalityScoreAtom);
+    const [flexabilityScore, setFlexabilityScore] = useRecoilState(flexabilityScoreAtom);
+    const [fluencyScore, setFluencyScore] = useRecoilState(fluencyScoreAtom);
+    const [submissions, setSubmissions] = useRecoilState(submissionsAtom);
+    const [name, setName] = useState('');
+    const handleChange = (event) => {
+      setName(event.target.value);
+    };
+    const alert = useAlert()
+
+    let nav = useNavigate();
+    
+
+    const navToComposePanel = () => {
+
+        if (name != '') {
+
+
+            let payload = {
+                data:name,
+                basic:"1"
+            }
+        
+
+       axios.post('https://fantasifreely.de/'+"startBasic",JSON.stringify(payload), {
+        headers: {
+            "Content-Type": "application/json"
+        }
+       }).then((response) => {
+            console.log(response.data)
+            setjwtToken(response.data)
+            setFlexabilityScore(0)
+            setFluencyScore(0)
+            setOriginalityScore(0)
+            setSubmissions(0)
+
+            nav("/Compose")
+            caches.keys().then((names) => {
+                names.forEach((name) => {
+                  caches.delete(name);
+                });
+              });
+
+        }).catch((error) => {
+            if(error.response.status == 408 ){
+                alert.show('Given Initial already exists. Please provide a different inital');  return
+            }else{
+            console.log(error)}
+        })
+
+    }
+        //nav("/Compose")
+
+        
+
+        else{
+            alert.show('Please put your initials in the Name Box');  return
+        }
+    }
+
+   
+
+    return (
+        <div>
+        <div style={{fontSize: "50px",textAlign:"center",fontFamily:"Cursive",height:"80px",width:"550px",marginLeft:"500px",marginTop:"30px", backgroundColor:"#403c3b","color":"white" }}>
+        Fantasize Freely
+    </div>
+    <div style={{alignItems:"center",display:"flex",justifyContent:"center"}} >
+        
+       
+        <div>
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '40ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="outlined-name"
+        label="Please type your initials here"
+        value={name}
+        onChange={handleChange}
+        style={{border: '3px solid #403c3b',backgroundColor:'white'}}
+      />
+      
+    </Box>
+    </div>
+       
+        <div style={{height:"900px"}}>
+        
+            
+        
+            <div style={{textAlign:"center",fontFamily:"Cursive",borderRadius:"30px",fontSize: "25px",height:"540px",width:"800px",marginLeft:"100px",marginTop:"50px", backgroundColor:"#403c3b","color":"white"}}>
+        <Typewriter options={{delay: 30}}
+        onInit={(typewriter) => {
+            typewriter.typeString("Welcome fellow Musician! On this platform you can test your creativity in melody composition. The main task is to compose 3-5 short melodies with a length of 8 to 16 measures. During composing you have the opportunity to listen to your melody but also you can play it on an acoustic instrument. An artificial intelligence can provide you with inspirations which are around one measure. But make sure all the previously composed measures are completely filled. Otherwise it will just reject your request. The provided material from the AI is presented to you in different forms (e.g. just showing the durations or a sequence of different strength of a color will be shown). On the bottom right 3 different creativity score are shown. They are updated in realtime while composing. When the time has come and you think your melody is ready for a submission just click on the submitComposition Button. I wish you a lot of fun!").start();
+        } }  
+        />
+            </div >
+            
+            <Button style={{height:"150px",padding:"0px",backgroundColor:"#403c3b",marginTop:"70px",marginLeft:"230px","borderRadius":"5px","font-weight": "bold","height":"50px","width":"255px","border":"gold 2px solid"}}><Link style={{color:'white',textDecoration: 'none'}} to="/tutorial" target="_blank">Tutorial</Link></Button>
+            <Button style={{height:"150px",padding:"0px",backgroundColor:"#403c3b",marginTop:"70px",marginLeft:"50px","borderRadius":"5px","font-weight": "bold","height":"50px","width":"265px","border":"gold 2px solid"}} onClick={navToComposePanel}>Start Composing</Button>
+
+        </div>
+    </div>
+
+       
+    </div>
+
+
+
+    )
+}
+
+
+export default Main;
---
+++ src/components/result.js
@@ -0,0 +1,136 @@
+import React ,{  useEffect,useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import { useRecoilState, useRecoilValue,useResetRecoilState } from 'recoil'
+import ScoreBox from './ScoreBox';
+import { measure1 as measure1Atom,measure2 as measure2Atom,
+    measure3 as measure3Atom,measure4 as measure4Atom,measure5 as measure5Atom,measure6 as measure6Atom,
+    measure7 as measure7Atom,measure8 as measure8Atom, musicatResponse as musicatResponseAtom,originalityScore as originalityScoreAtom,
+    fluencyScore as fluencyScoreAtom,flexabilityScore as flexabilityScoreAtom,
+    submissions as submissionsAtom, jwtToken as jwtTokenAtom,
+analogies as analogiesAtom, groups as groupsAtom, totalResult as totalResultAtom} from '../redux/store'
+    
+import { Progress } from 'react-sweet-progress';
+import { Button } from '@mui/material';
+
+
+
+
+function Result() {
+
+    let nav = useNavigate();
+    const maxTotalResult = 400
+    
+    const musicatResult = useRecoilValue(musicatResponseAtom);
+    const analogies = useRecoilValue(analogiesAtom);
+    const groups = useRecoilValue(groupsAtom);
+    const totalResult = useRecoilValue(totalResultAtom);
+
+    
+    const [image, setImage] = useState("");
+    const [submissions, setSubmissions] = useRecoilState(submissionsAtom);
+    const [flexabilityScore, setFlexabilityScore] = useRecoilState(flexabilityScoreAtom);
+    const [fluencyScore, setFluencyScore] = useRecoilState(fluencyScoreAtom);
+    const [originalityScore, setOriginalityScore] = useRecoilState(originalityScoreAtom);
+    const [jwtToken, setJwtToken] = useRecoilState(jwtTokenAtom);
+    const [creativityCategory, setCreativityCategory] = useState(null);
+
+
+
+    const nextComposition = () => {
+        setFlexabilityScore(0)
+        setFluencyScore(0)
+        setOriginalityScore(0)
+        nav("/compose")
+    }
+
+    const endTask = () => {
+        setJwtToken([])
+        setSubmissions(0)
+        setFlexabilityScore(0)
+        setFluencyScore(0)
+        setOriginalityScore(0)
+        nav("/")
+        
+    }
+
+    useEffect(() => {
+
+        console.log(totalResult)
+        if (totalResult < 200) {
+            setCreativityCategory(0)
+            
+        }
+        else if(totalResult >= 200 && totalResult < 350) {
+            setCreativityCategory(1)
+        }
+        else if(totalResult >= 350) {
+            setCreativityCategory(2)
+        }
+        setImage("data:image/jpeg;charset=utf-8;base64,"+musicatResult)
+        console.log(creativityCategory)
+
+
+    },[musicatResult])
+    
+
+
+    return (
+
+        <div>
+            <div style={{backgroundColor:"#debd90" ,marginBottom:"30px",fontSize:"40px","fontWeight": "bold",textAlign:"center"}}> Grading your Composition  </div>
+
+            <div style={{borderRadius:"30px",border:"dashed 10px #debd90",float:"left", margin:"25px",backgroundColor:"whitesmoke"}}>
+           <div style={{marginTop:"30px",fontSize:"25px","fontWeight": "bold",textAlign:"center"}}> Musicats Listening Result </div>
+                <img
+                    width={1200}
+                    height={700}
+                    src= {image}
+                />
+                
+            
+            </div>
+            <div>
+                <div>
+            <div style={{border:"solid 4px silver",borderRadius:"20px",backgroundColor:"#debd90" ,width:"400px", height:"300px", marginTop:"100px",alignItems:"center",display:"flex",justifyContent:"center"}}>
+                <div style={{float:"left"}}>
+                   
+            <div style={{float:"left"}}>
+                    <div style={{borderRadius:"8px",textAlign: "center",height:"30px",width:"130px","fontWeight": "bold",backgroundColor:"#399ddb" ,marginBottom:"30px"}}>
+                    Creativity Score
+                    </div>
+            <Progress  type="circle" percent={(Math.floor((totalResult/maxTotalResult)*100))}  />
+            </div>
+            
+            </div>
+            </div>
+            
+            </div>
+            {submissions < 5 && <Button onClick={nextComposition} style={{margin:"50px","fontWeight": "bold","borderRadius":"5px","color":"white","height":"50px","backgroundColor":"#403c3b","border":"#403c3b 2px solid"}}>Another Composition</Button>}
+            {submissions > 3 && <Button onClick={endTask} style={{margin:"50px","fontWeight": "bold","borderRadius":"5px","color":"white","height":"50px","backgroundColor":"#403c3b","border":"#403c3b 2px solid"}}>End Task</Button>}
+
+
+            </div>
+            <div style={{backgroundColor:"#debd90",height:"350px"}}>
+            {creativityCategory==2 && <div>    <span style={{fontSize: "200px", float:"left"}}>🤩</span>
+                        <span style={{"fontWeight": "bold",fontSize: "50px"}}>WOW! That was highly creative. Congratulation!</span>
+                        </div>}
+            {creativityCategory==1 && <div>    <span style={{fontSize: "200px", float:"left"}}>😐</span>
+                    <span style={{"fontWeight": "bold",fontSize: "40px"}}>That was moderately creative. Good Job but there is room for improvement! </span>
+                    </div>}
+            {creativityCategory==0 && <div>    <span style={{fontSize: "200px", float:"left"}}>😟</span>
+                    <span style={{"fontWeight": "bold",fontSize: "40px"}}>That was not creative at all. You have to work on yourself!</span>
+                    </div>}
+
+                    </div>
+            
+        </div>
+
+
+    );
+
+
+    }
+
+
+
+export default Result;
